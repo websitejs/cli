44aed0d91609b8522376e8a0457117fdcef62581
Rocco Janse <rocco.janse@valtech.nl>
2017-04-20 00:24:17 +0200

    Fixed filepath creation and added file exclusion

--- src/files.js
+++ src/files.js
@@ -41,117 +41,137 @@ var Files = module.exports = {
         var tplLocation = path.join(Files.cwd, typeTplPath),
             destPath = path.join(Files.cwd, typeDestPath),
             fileTypes = ['scss', 'html', 'js', 'gspec'],
-            processed = 0,
+            filesToProcess = 0,
+            filesProcessed = 0,
             errors = 0;
 
-        fileTypes.forEach(function(fType, i) {
+        // format filename
+        name = name.toLowerCase();
 
-            name = name.toLowerCase();
+        // sanity filename check
+        // only accept alphanumeric chars, dashes and underscores
+        // but also allow forward slash to create files in subdirs
+        var regex = /^[0-9A-Za-z\-\_\/]+$/,
+            illegalName = regex.test(name);
 
-            var regex = /^[0-9A-Za-z\-\_\/]+$/,
-                illegalName = regex.test(name);
+        if (!illegalName) {
+            console.error('\nError: Only 0-9, a-z, A-Z, - and _ are allowed in the name of the %s.\n\n', type);
+            process.exit(1);
+        }
 
-            // sanity filename check
-            if (!illegalName) {
-                console.log('\nError: Only 0-9, a-z, A-Z, - and _ are allowed in the name of the %s.\n\n', type);
+        // create path when slashes in filename
+        var resolvedPath = name.split('/');
+        if (resolvedPath.length > 0) {
+
+            // strip off name of element/component and store it
+            name = resolvedPath.splice(-1, 1)[0];
+
+            // create new destination path
+            destPath = path.resolve(path.join(destPath, resolvedPath.join('/')));
+
+            // try to create path
+            try {
+                fs.mkdirsSync(destPath, '0777');
+                console.log('\n%s created.', destPath);
+            } catch (err) {
+                console.error(err);
                 process.exit(1);
             }
+        }
 
-            // create path when slashes in filename 
-            var resolvedPath = name.split('/');
-            if (resolvedPath.length > 0) {
-                // we need to create a path
-                name = resolvedPath.splice(-1, 1)[0];
-                destPath = destPath + '/' + resolvedPath.join('/');
+        // create files for each type
+        fileTypes.forEach(function(fType, i) {
 
-                fs.mkdirs(destPath, '0777', function() {
-                    console.log('Created dir: ', destPath);
-                }); 
-            }
+            // if option of current filetype is not set
+            if (typeof options[fType] === 'undefined') {
 
-            var filename = name;
-            if (fType === 'scss') {
-                filename = '_' + name;
-            }
+                filesToProcess++;
 
-            var tplFile = path.resolve(tplLocation, fType + '.tpl'),
-                destFile = path.resolve(destPath, name, filename + '.' + fType);
-
-            fs.readFile(tplFile, 'utf-8', function(err, data) {
-
-                if (err) {
-                    console.error('\nError: cannot read template: "' + tplFile + '".\n\n');
-                    process.exit(1);
+                var filename = name;
+                if (fType === 'scss') {
+                    filename = '_' + name;
                 }
 
-                // replace template values
-                var d = new Date(),
-                    months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+                var tplFile = path.resolve(tplLocation, fType + '.tpl'),
+                    destFile = path.resolve(destPath, name, filename + '.' + fType);
 
-                // get users 'full name'
-                fullname().then(function(uName) {
+                fs.readFile(tplFile, 'utf-8', function(err, data) {
 
-                    // or fallback to 'username' and try to format it
-                    if (!uName) {
-                        var userNameArray = username.sync().split('.');
-                        userNameArray[0].charAt(0).toUpperCase();
-                        userNameArray[(userNameArray.length - 1)].charAt(0).toUpperCase();
-                        uName = userNameArray.join(' ');
+                    if (err) {
+                        console.error('\nError: cannot read template: "' + tplFile + '".\n\n');
+                        process.exit(1);
                     }
 
-                    // capitalize name
-                    var jsName = name.charAt(0).toUpperCase() + name.slice(1);
+                    // replace template values
+                    var d = new Date(),
+                        months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
 
-                    if (fType === 'js') {
-                        // format name as js class
-                        // TODO: this should be more elegant
-                        var jsNameArray = name.split('-');
-                        for (var j = 0; j < jsNameArray.length; j++) {
-                            if (jsNameArray[j] !== '') {
-                                jsNameArray[j] = jsNameArray[j].charAt(0).toUpperCase() + jsNameArray[j].slice(1);
+                    // get users 'full name'
+                    fullname().then(function(uName) {
+
+                        // or fallback to 'username' and try to format it
+                        if (!uName) {
+                            var userNameArray = username.sync().split('.');
+                            userNameArray[0].charAt(0).toUpperCase();
+                            userNameArray[(userNameArray.length - 1)].charAt(0).toUpperCase();
+                            uName = userNameArray.join(' ');
+                        }
+
+                        // capitalize name
+                        var jsName = name.charAt(0).toUpperCase() + name.slice(1);
+
+                        if (fType === 'js') {
+                            // format name as js class
+                            // TODO: this should be more elegant
+                            var jsNameArray = name.split('-');
+                            for (var j = 0; j < jsNameArray.length; j++) {
+                                if (jsNameArray[j] !== '') {
+                                    jsNameArray[j] = jsNameArray[j].charAt(0).toUpperCase() + jsNameArray[j].slice(1);
+                                }
                             }
-                        }
-                        jsNameArray = jsNameArray.join('').split('_');
-                        for (var k = 0; k < jsNameArray.length; k++) {
-                            if (jsNameArray[k] !== '') {
-                                jsNameArray[k] = jsNameArray[k].charAt(0).toUpperCase() + jsNameArray[k].slice(1);
+                            jsNameArray = jsNameArray.join('').split('_');
+                            for (var k = 0; k < jsNameArray.length; k++) {
+                                if (jsNameArray[k] !== '') {
+                                    jsNameArray[k] = jsNameArray[k].charAt(0).toUpperCase() + jsNameArray[k].slice(1);
+                                }
                             }
-                        }
-                        jsName = jsNameArray.join('');
-                    }
-
-                    // replace tpl values
-                    data = data.replace(/{{name}}/gi, name);
-                    data = data.replace(/{{ucfirst_name}}/gi, jsName);
-                    data = data.replace(/{{author}}/gi, uName);
-                    data = data.replace(/{{date}}/gi, months[d.getMonth()] + ' ' + d.getFullYear());
-
-                    // write file
-                    fs.outputFile(destFile, data, function(err) {
-
-                        processed++;
-
-                        if (err) {
-                            console.log(err);
-                            errors++;
-                        } else {
-                            // set files to full access
-                            fs.chmodSync(destFile, '0777');
-                            console.log('Created ' + destFile + '.');
+                            jsName = jsNameArray.join('');
                         }
 
-                        if (processed === fileTypes.length) {
-                            if (errors > 0) {
-                                console.log('Done, but with errors (%d).', errors);
-                                process.exit(1);
+                        // replace tpl values
+                        data = data.replace(/{{name}}/gi, name);
+                        data = data.replace(/{{ucfirst_name}}/gi, jsName);
+                        data = data.replace(/{{author}}/gi, uName);
+                        data = data.replace(/{{date}}/gi, months[d.getMonth()] + ' ' + d.getFullYear());
+
+                        // write file
+                        fs.outputFile(destFile, data, function(err) {
+
+                            filesProcessed++;
+
+                            if (err) {
+                                console.log(err);
+                                errors++;
                             } else {
-                                console.log('Done, succesfully created %s "%s".', type, name);
-                                process.exit();
+                                // set files to full access
+                                fs.chmodSync(destFile, '0777');
+                                console.log('%s created.', destFile);
                             }
-                        }
+
+                            if (filesProcessed === filesToProcess) {
+                                if (errors > 0) {
+                                    console.error('\nDone, but with errors (%d).', errors);
+                                    process.exit(1);
+                                } else {
+                                    console.log('\nDone, succesfully created %s "%s".', type, name);
+                                    process.exit();
+                                }
+                            }
+                        });
                     });
                 });
-            });
+            }
+
         });
     }
 };
